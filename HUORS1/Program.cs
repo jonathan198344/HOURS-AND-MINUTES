d1a00711aa62baa5575d1cabcc5d928c106832ce
jonathan198344 <jonathan_-1983@outlook.com>
2026-02-25 19:03:40 -0500

    createprojec

--- HUORS1/Program.cs
+++ HUORS1/Program.cs
@@ -1,179 +1,33 @@
-﻿using System;
+﻿using bakent;
 
-public class Time
+try
 {
-    private int hour;
-    private int minute;
-    private int second;
-    private int millisecond;
 
-    // Constructor with no parameters
-    public Time()
+    var t1 = new Time();
+    var t2 = new Time(14);
+    var t3 = new Time(9, 34);
+    var t4 = new Time(19, 45, 56);
+    var t5 = new Time(23, 3, 45, 678);
+
+
+    var times = new List<Time> { t1, t2, t3, t4, t5 };
+
+    foreach (Time time in times)
     {
-        hour = 0;
-        minute = 0;
-        second = 0;
-        millisecond = 0;
+        Console.WriteLine($"Time: {time}");
+        Console.WriteLine($"\tMilliseconds: {time.ToMilliseconds(),15:N0}");
+        Console.WriteLine($"\tSeconds     : {time.ToSeconds(),15:N0}");
+        Console.WriteLine($"\tMinutes     : {time.ToMinutes(),15:N0}");
+        Console.WriteLine($"\tAdd         : {time.Add(t3),15:N0}");
+        Console.WriteLine($"\tIs Other day: {time.IsOtherDay(t4)}");
+        Console.WriteLine();
     }
 
-    // Constructor with hours
-    public Time(int h) : this(h, 0, 0, 0)
-    {
-    }
 
-    // Constructor with hours and minutes
-    public Time(int h, int m) : this(h, m, 0, 0)
-    {
-    }
-
-    // Constructor with hours, minutes and seconds
-    public Time(int h, int m, int s) : this(h, m, s, 0)
-    {
-    }
-
-    // Constructor with hours, minutes, seconds and milliseconds
-    public Time(int h, int m, int s, int ms)
-    {
-        if (h < 0 || h > 23)
-            throw new ArgumentException("Invalid hour");
-
-        if (m < 0 || m > 59)
-            throw new ArgumentException("Invalid minute");
-
-        if (s < 0 || s > 59)
-            throw new ArgumentException("Invalid second");
-
-        if (ms < 0 || ms > 999)
-            throw new ArgumentException("Invalid millisecond");
-
-        hour = h;
-        minute = m;
-        second = s;
-        millisecond = ms;
-    }
-
-    // Returns time in AM/PM format
-    public override string ToString()
-    {
-        int displayHour = hour;
-        string period = "AM";
-
-        if (hour >= 12)
-        {
-            period = "PM";
-            if (hour > 12)
-                displayHour = hour - 12;
-        }
-
-        if (displayHour == 0)
-            displayHour = 12;
-
-        return displayHour.ToString("00") + ":" +
-               minute.ToString("00") + ":" +
-               second.ToString("00") + "." +
-               millisecond.ToString("000") + " " +
-               period;
-    }
-
-    // Returns total milliseconds
-    public long ToMilliseconds()
-    {
-        return (hour * 3600000L) +
-               (minute * 60000L) +
-               (second * 1000L) +
-               millisecond;
-    }
-
-    // Returns total seconds
-    public long ToSeconds()
-    {
-        return (hour * 3600L) +
-               (minute * 60L) +
-               second;
-    }
-
-    // Returns total minutes
-    public long ToMinutes()
-    {
-        return (hour * 60L) + minute;
-    }
-
-    // Checks if adding another Time passes to next day
-    public bool IsOtherDay(Time other)
-    {
-        long totalMilliseconds = this.ToMilliseconds() + other.ToMilliseconds();
-        long millisecondsInDay = 24L * 60L * 60L * 1000L;
-
-        return totalMilliseconds >= millisecondsInDay;
-    }
-
-    // Adds two Time objects
-    public Time Add(Time other)
-    {
-        int newMillisecond = this.millisecond + other.millisecond;
-        int extraSecond = newMillisecond / 1000;
-        newMillisecond = newMillisecond % 1000;
-
-        int newSecond = this.second + other.second + extraSecond;
-        int extraMinute = newSecond / 60;
-        newSecond = newSecond % 60;
-
-        int newMinute = this.minute + other.minute + extraMinute;
-        int extraHour = newMinute / 60;
-        newMinute = newMinute % 60;
-
-        int newHour = this.hour + other.hour + extraHour;
-        newHour = newHour % 24;
-
-        return new Time(newHour, newMinute, newSecond, newMillisecond);
-    }
+    var t6 = new Time(45, -7, 90, -87);
 }
-
-class Program
+catch (Exception exception)
 {
-    static void Main(string[] args)
-    {
-        try
-        {
-            Time t1 = new Time();
-            Time t2 = new Time(10);
-            Time t3 = new Time(10, 30);
-            Time t4 = new Time(23, 58, 34, 666);
-            Time t5 = new Time(5, 15, 45, 120);
 
-            Time[] times = { t1, t2, t3, t4, t5 };
-
-            Console.WriteLine("Original times:\n");
-
-            foreach (Time t in times)
-            {
-                Console.WriteLine("Time: " + t.ToString());
-                Console.WriteLine("Milliseconds: " + t.ToMilliseconds());
-                Console.WriteLine("Seconds: " + t.ToSeconds());
-                Console.WriteLine("Minutes: " + t.ToMinutes());
-                Console.WriteLine();
-            }
-
-            Console.WriteLine("Adding t3:\n");
-
-            foreach (Time t in times)
-            {
-                Time result = t.Add(t3);
-                Console.WriteLine(t.ToString() + " + " + t3.ToString() + " = " + result.ToString());
-            }
-
-            Console.WriteLine("\nChecking if it passes to next day with t4:\n");
-
-            foreach (Time t in times)
-            {
-                Console.WriteLine(t.ToString() + " passes to next day: " + t.IsOtherDay(t4));
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error: " + ex.Message);
-        }
-
-        Console.ReadLine();
-    }
+    Console.WriteLine(exception.Message);
 }
---
+++ bakent/Time.cs
@@ -0,0 +1,228 @@
+﻿namespace bakent;
+
+public class Time
+{
+
+
+
+
+    private int _hour;
+    private int _minute;
+    private int _second;
+    private int _millisecond;
+
+    // Constructor with no parameters
+    public Time()
+    {
+        _hour = 0;
+        _minute = 0;
+        _second = 0;
+        _millisecond = 0;
+    }
+
+    // Constructor with hours
+    public Time(int hour) 
+    {
+        Hour = hour;
+    }
+
+    // Constructor with hours and minutes
+    public Time(int hour, int minute)
+    {
+        Hour = hour;
+        Minute = minute;
+    }
+
+    // Constructor with hours, minutes and seconds
+    public Time(int hour, int minute, int second)
+    {
+        Hour = hour;
+        Minute = minute;
+        Second = second;
+    }
+
+    // Constructor with hours, minutes, seconds and milliseconds
+    public Time(int hour, int minute, int second, int milisecond)
+    {
+
+        Hour = hour;
+        Minute = minute;
+        Second = second;
+        Millisecond = milisecond;
+    }
+
+    public int Hour
+    { 
+    get => _hour;
+        set => _hour = ValidHour(value);
+
+    }
+
+    public int Minute
+    {
+        get => _minute;
+        set => _minute = ValidMinute(value);
+    }   
+    public int Second
+    {
+        get => _second;
+        set => _second = ValidSecond(value);
+
+    }
+
+    public int Millisecond
+    {
+        get => _millisecond;
+        set => _millisecond = ValidMillisecond(value);
+    }
+    // Returns time in AM/PM format
+    public override string ToString()
+    {
+        if (Hour < 0 || Hour > 23)
+            throw new ArgumentOutOfRangeException("Ithe time entered is not valid");
+
+        if (Minute < 0 || Minute > 59)
+            throw new ArgumentOutOfRangeException("Ithe time entered is not valid");
+
+        if (Second < 0 || Second > 59)
+            throw new ArgumentOutOfRangeException("Ithe time entered is not valid");
+
+        if (Millisecond < 0 || Millisecond > 999)
+            throw new ArgumentOutOfRangeException("Ithe time entered is not valid");
+        int displayHour = Hour;
+        string period = "AM";
+
+        if (Hour >= 12)
+        {
+            period = "PM";
+            if (Hour > 12)
+                displayHour = Hour - 12;
+        }
+
+        if (displayHour == 0)
+            displayHour = 12;
+
+        return $"{displayHour:00}:{Minute:00}:{Second:00}.{Millisecond:000} {period}"; 
+    }
+
+    // Returns total milliseconds
+    public int ToMilliseconds()
+    {
+
+        if (Hour < 0 || Hour > 23)
+            return 0;
+
+        if (Minute < 0 || Minute > 59)
+            return 0;
+
+        if (Second < 0 || Second > 59)
+            return 0;
+
+        if (Millisecond < 0 || Millisecond > 999)
+           return 0;
+        return (Hour * 3600000) +
+               (Minute * 60000) +
+               (Second * 1000) +
+               Millisecond;
+    }
+
+    // Returns total seconds
+    public int ToSeconds()
+    {
+
+        if (Hour < 0 || Hour > 23)
+            return 0;
+
+        if (Minute < 0 || Minute > 59)
+            return 0;
+
+        if (Second < 0 || Second > 59)
+            return 0;
+
+        if (Millisecond < 0 || Millisecond > 999)
+            return 0;
+        return (Hour * 3600) +
+               (Minute * 60) +
+               Second + 
+               (Millisecond / 1000); 
+    }
+
+    // Returns total minutes
+    public int ToMinutes()
+    {
+
+        if (Hour < 0 || Hour > 23)
+            return 0;
+
+        if (Minute < 0 || Minute > 59)
+            return 0;
+
+        if (Second < 0 || Second > 59)
+            return 0;
+
+        if (Millisecond < 0 || Millisecond > 999)
+            return 0;
+        return (Hour * 60) + Minute + (Second / 60) + (Millisecond / 60000);
+    }
+
+    // Checks if adding another Time passes to next day
+    public bool IsOtherDay(Time other)
+    {
+        int totalMilliseconds = this.ToMilliseconds() + other.ToMilliseconds();
+         if (totalMilliseconds >= 86400000 )
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    // Adds two Time objects
+    public Time Add(Time other)
+    {
+        int TotalMillisecond = this.ToMilliseconds()+ other.ToMilliseconds();
+
+        TotalMillisecond %= 86400000;
+        int newHour = (TotalMillisecond / 3600000);
+        int newMinute = (TotalMillisecond / 60000) % 60;
+        int newSecond = (TotalMillisecond / 1000) % 60;
+        int newMillisecond = TotalMillisecond % 1000;
+
+        return new Time(newHour, newMinute, newSecond, newMillisecond);
+    }
+    private int ValidHour(int hour)
+    {
+        if (hour < 0 || hour > 23)
+        {
+            throw new ArgumentOutOfRangeException(nameof(hour), $"The hour: {hour}, is not valid.");
+        }
+        return hour;
+    }
+
+
+    private int ValidMinute(int minute)
+    {
+        if (minute < 0 || minute > 59)
+        {
+            throw new ArgumentOutOfRangeException(nameof(minute), $"The minute: {minute}, is not valid.");
+        }
+        return minute;
+    }
+    private int ValidSecond(int second)
+    {
+        if (second < 0 || second > 59)
+        {
+            throw new ArgumentOutOfRangeException(nameof(second), $"The second: {second}, is not valid.");
+        }
+        return second;
+    }
+    private int ValidMillisecond(int millisecond)
+    {
+        if (millisecond < 0 || millisecond > 999)
+        {
+            throw new ArgumentOutOfRangeException(nameof(millisecond), $"The millisecond: {millisecond}, is not valid.");
+        }
+        return millisecond;
+    }
+}
